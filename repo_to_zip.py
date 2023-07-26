52cbdd05ee0d41efc6d53f925d0c30fa71c3dcc2
isayahc <isayahculbertson@gmail.com>
2023-07-26 17:19:08 -0400

    modified args

--- repo_to_zip.py
+++ repo_to_zip.py
@@ -3,13 +3,14 @@ import shutil
 import os
 import argparse
 
-def download_repo_zip(url, filename):
+def download_repo_zip(url, filepath):
     if not url.endswith('/'):
         url += '/'
     archive_url = url + 'archive/main.zip'
     response = requests.get(archive_url, stream=True)
-    cwd = os.getcwd()
-    save_path = os.path.join(cwd, filename)
+    output_dir, filename = os.path.split(filepath)  # Split the filepath into a directory path and filename
+    os.makedirs(output_dir, exist_ok=True)  # Create the output directory if it doesn't exist
+    save_path = filepath  # Save the file at the specified filepath
     with open(save_path, 'wb') as out_file:
         shutil.copyfileobj(response.raw, out_file)
     del response
@@ -17,12 +18,9 @@ def download_repo_zip(url, filename):
 def main():
     parser = argparse.ArgumentParser(description='Download a GitHub repository as a ZIP file.')
     parser.add_argument('url', help='The URL of the GitHub repository.')
-    parser.add_argument('filename', nargs='?', help='The filename for the downloaded ZIP file.')
+    parser.add_argument('filepath', help='The file path (including filename) where the downloaded ZIP file should be saved.')
     args = parser.parse_args()
-    if args.filename is None:
-        repo_name = args.url.split('/')[-1] if args.url[-1] != '/' else args.url.split('/')[-2]
-        args.filename = repo_name + '.zip'
-    download_repo_zip(args.url, args.filename)
+    download_repo_zip(args.url, args.filepath)
 
 if __name__ == '__main__':
     main()
--- web_to_zip.py
+++ web_to_zip.py
@@ -21,6 +21,7 @@ def download_webpage(url, path='webpage'):
         return None, None
 
 def zip_files(files, zip_name):
+    os.makedirs(os.path.dirname(zip_name), exist_ok=True)  # Create the output directory if it doesn't exist
     with zipfile.ZipFile(zip_name, 'w') as zipf:
         for file in files:
             zipf.write(file)
@@ -28,10 +29,12 @@ def zip_files(files, zip_name):
 def main():
     parser = argparse.ArgumentParser(description="Download and zip a webpage.")
     parser.add_argument('url', help='The URL of the webpage to download.')
+    parser.add_argument('output_dir', nargs='?', default=os.getcwd(), help='The directory to save the downloaded ZIP file.')
     args = parser.parse_args()
     file, title = download_webpage(args.url)
     if file is not None:
-        zip_files([file], f'{title}.zip')
+        zip_name = os.path.join(args.output_dir, f'{title}.zip')  # Save the file in the output directory
+        zip_files([file], zip_name)
 
 if __name__ == "__main__":
     main()
