606779941341990f5f930d204d647a480994ff3b
isayahc <isayahculbertson@gmail.com>
2023-07-26 17:43:40 -0400

    modified for flexiblity

--- repo_to_zip.py
+++ repo_to_zip.py
@@ -8,6 +8,14 @@ def download_repo_zip(url, filepath):
         url += '/'
     archive_url = url + 'archive/main.zip'
     response = requests.get(archive_url, stream=True)
+
+    # Check if filepath is a directory
+    if os.path.isdir(filepath):
+        # Get the repository name from the URL
+        repo_name = url.split('/')[-1] if url[-1] != '/' else url.split('/')[-2]
+        # Append the repository name to the directory path
+        filepath = os.path.join(filepath, repo_name + '.zip')
+    
     output_dir, filename = os.path.split(filepath)  # Split the filepath into a directory path and filename
     os.makedirs(output_dir, exist_ok=True)  # Create the output directory if it doesn't exist
     save_path = filepath  # Save the file at the specified filepath
